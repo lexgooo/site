56595707fdb7d5db13e89b04234351193f3fa0ca
chao.gu <chao.gu@daoyitong.com>
2019-09-06 17:47:19 +0800

    修改打包引用路径

---
+++ docs/precache-manifest.881d89ea8ede0be210b5c417eca26676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30e25425b2c9f04bc835dc2590ed2f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de53f8c8cdacf4e94866",
+    "url": "./static/css/2.6c4ac53e.chunk.css"
+  },
+  {
+    "revision": "08fe3225417bd707d0a8",
+    "url": "./static/css/main.5f0ffa2c.chunk.css"
+  },
+  {
+    "revision": "de53f8c8cdacf4e94866",
+    "url": "./static/js/2.868bb7f4.chunk.js"
+  },
+  {
+    "revision": "08fe3225417bd707d0a8",
+    "url": "./static/js/main.3813087b.chunk.js"
+  },
+  {
+    "revision": "5170d2b1c0ede261d572",
+    "url": "./static/js/runtime~main.1e9a1a91.js"
+  }
+]);
